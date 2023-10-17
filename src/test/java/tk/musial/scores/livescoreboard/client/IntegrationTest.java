022353d2359cddfc4d6b7586fca0b5f6ddc2bcc2
grzegorz@musial.tk <grzegorz@musial.tk>
2023-10-17 23:39:37 +0200

    test: initial integration test

--- src/test/java/tk/musial/scores/livescoreboard/client/IntegrationTest.java
+++ src/test/java/tk/musial/scores/livescoreboard/client/IntegrationTest.java
@@ -1,6 +1,6 @@
 package tk.musial.scores.livescoreboard.client;
 
-import java.util.List;
+import static org.fest.assertions.Assertions.assertThat;
 
 import org.junit.jupiter.api.Test;
 
@@ -14,14 +14,35 @@ public class IntegrationTest {
 
     @Test
     void playWorldCup() {
-        ScoreBoard board = new InMemoryScoreBoard();
+        var board = new InMemoryScoreBoard();
 
-        Match match = new Match(new Team("Mexico"), new Team("Canada"));
-        match.start(board)
-          .updateScore(new Score(1), new Score(0))
-          .updateScore(new Score(2), new Score(0))
-          .finish();
+        var mexCan = new Match(new Team("Mexico"), new Team("Canada"));
+        board.start(mexCan)
+          .updateScore(new Score(0, 5));
 
-        List<String> summary = board.getSummary();
+        var spaBra = new Match(new Team("Spain"), new Team("Brazil"));
+        board.start(spaBra)
+          .updateScore(new Score(10, 2));
+
+        var gerFra = new Match(new Team("Germany"), new Team("France"));
+        board.start(gerFra)
+          .updateScore(new Score(2,2));
+
+        var uruIta = new Match(new Team("Uruguay"), new Team("Italy"));
+        board.start(uruIta)
+          .updateScore(new Score(6,6));
+
+        var argAus = new Match(new Team("Argentina"), new Team("Australia"));
+        board.start(argAus)
+          .updateScore(new Score(3,1));
+
+
+        assertThat(board.getSummary()).containsExactly(
+          "Uruguay 6 - Italy 6",
+          "Spain 10 - Brazil 2",
+          "Mexico 0 - Canada 5",
+          "Argentina 3 - Australia 1",
+          "Germany 2 - France 2"
+        );
     }
 }
